35911c38602910a96d9396914882f5fc6d64a8cc
Patricio Uskaer <patriciouskaer@gmail.com>
2023-06-25 00:01:47 -0300

    hover

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
         <div className='h-screen flex flex-col items-center bg-white '>
           <div id="container" className="container flex flex-col p-2 items-center gap-4 ">
             <button
-              className='bg-lime-500 p-2 rounded-md mt-2  shadow-md border-gray-900'
+              className='bg-lime-500 p-2 rounded-md mt-2  shadow-md border-gray-900 hover:bg-lime-400'
               onClick={toggleDarkMode}>{darkMode ? <i className="bi bi-lamp bi-3x"></i> : <i className="bi bi-lamp-fill bi-3x"></i>}</button>
             <h2 className="title  font-bold text-2xl bg-blue-700  shadow-md p-2 rounded-md w-full text-white text-center ">
               Random Quote Machine<i class="bi bi-chat-quote-fill ml-2"></i>
@@ -44,7 +44,7 @@ function App() {
           </div>
           <div id="wrapper" className='  rounded-md m-2 bg-lime-500 shadow-md'>
             <div className="buttons text-center m-4  ">
-              <button className='butt mr-4 text-md  bg-blue-700 rounded-md p-2 font-semibold text-white' id="new-quote" onClick={getQuote}>
+              <button className='butt mr-4 text-md  bg-blue-700 rounded-md p-2 font-semibold text-white hover:bg-blue-600' id="new-quote" onClick={getQuote}>
                 New Quote
               </button>
             </div>
@@ -74,7 +74,7 @@ function App() {
               rel="noreferrer"
               id="tweet-quote"
             >
-              <i className="fa fa-twitter text-4xl text-blue-700 mt-2 "><p className='text-sm'>Share on Twitter!</p></i>
+              <i className="fa fa-twitter text-4xl text-blue-700 mt-2 hover:text-blue-600"><p className='text-sm '>Share on Twitter!</p></i>
             </a>
           </div>
           <p className="text-xs mb-md-0 text-center mt-2">
@@ -86,7 +86,7 @@ function App() {
         <div className='h-screen flex flex-col items-center bg-gray-900 '>
           <div id="container" className="container flex flex-col p-2 items-center gap-4 ">
             <button
-              className='bg-blue-700 p-2 rounded-md mt-2  shadow-md border-gray-900'
+              className='bg-blue-700 p-2 rounded-md mt-2  shadow-md border-gray-900 hover:bg-blue-600'
               onClick={toggleDarkMode}>{darkMode ? <i className="bi bi-lamp bi-3x"></i> : <i className="bi bi-lamp-fill bi-3x"></i>}</button>
             <h2 className="title  font-bold text-2xl bg-lime-500  shadow-md p-2 rounded-md w-full text-gray-900 text-center">
               Random Quote Machine<i class="bi bi-chat-quote-fill ml-2"></i>
@@ -94,7 +94,7 @@ function App() {
           </div>
           <div id="wrapper" className='  rounded-md m-2 bg-blue-700 shadow-md'>
             <div className="buttons text-center m-4  ">
-              <button className='butt mr-4 text-md  bg-lime-500 rounded-md p-2 font-semibold text-gray-900' id="new-quote" onClick={getQuote}>
+              <button className='butt mr-4 text-md  bg-lime-500 rounded-md p-2 font-semibold text-gray-900 hover:bg-lime-400' id="new-quote" onClick={getQuote}>
                 New Quote
               </button>
             </div>
@@ -124,7 +124,7 @@ function App() {
               rel="noreferrer"
               id="tweet-quote"
             >
-              <i className="fa fa-twitter text-4xl text-lime-500 mt-2 "><p className='text-sm'>Share on Twitter!</p></i>
+              <i className="fa fa-twitter text-4xl text-lime-500 mt-2 hover:text-lime-400"><p className='text-sm'>Share on Twitter!</p></i>
             </a>
           </div>
           <p className="text-xs mb-md-0 text-center mt-2 text-white">
